f851b72cecd0dc124e3959a42aed351ea30193ed
Caden <caden.chock@menloschool.org>
2024-02-02 13:46:05 -0800

    :)

--- src/Square.java
+++ src/Square.java
@@ -1,3 +1,7 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.ImageObserver;
+
 /**
  * A class written to support the TicTacToe Game.
  *
@@ -17,6 +21,22 @@ public class Square {
     private int col;
     private boolean isWinningSquare;
 
+    private final  static int sideLength = 100;
+    private final static int xBuffer = 100;
+    private final static int yBuffer = 100;
+    private final static int wBuffer = 23;
+    private final static int uBuffer = 50;
+    private final static int dBuffer = 150;
+
+    private final static int iSize = 80;
+    private final static int iBuffer = 110;
+
+    Image x;
+
+    Image o;
+    Square[][] b;
+
+
     /**
      * Constructor to initialize one Square of the
      * TicTacToe board
@@ -29,6 +49,8 @@ public class Square {
 
         this.marker = TicTacToe.BLANK;
         this.isWinningSquare = false;
+        o = new ImageIcon("Resources/Spheal.png").getImage();
+        x = new ImageIcon("Resources/Crobat.png").getImage();
     }
 
     /******************** Getters and Setters ********************/
@@ -57,5 +79,58 @@ public class Square {
      */
     public String toString() {
         return this.marker;
+    }
+
+    public static void drawBoard(Graphics g){
+        g.setColor(Color.black);
+        g.setFont(new Font("Serif", Font.PLAIN, 30));
+        for(int i = 0; i < 3; i++){
+            g.drawString(String.valueOf(i), uBuffer, dBuffer + (yBuffer*i));
+            g.drawString(String.valueOf(i), dBuffer + (xBuffer*i),uBuffer+wBuffer);
+            for(int j = 0; j< 3; j++){
+                //Draw Images for X and O
+                g.drawRect(xBuffer + (j*sideLength),yBuffer + (i*sideLength),sideLength,sideLength);
+            }
+        }
+    }
+
+    public void drawXO(Graphics g, TicTacToeViewer t){
+                //Draw Images for X and O
+                if(marker.equals(TicTacToe.O_MARKER)){
+                    g.drawImage(o, iBuffer + (xBuffer*col), iBuffer + (yBuffer*row), iSize, iSize, t);
+                }
+                if(marker.equals(TicTacToe.X_MARKER)) {
+                    g.drawImage(x, iBuffer + (xBuffer * col), iBuffer + (yBuffer * row), iSize, iSize, t);
+                }
+    }
+
+    public static void drawnWin(Graphics g, TicTacToe t){
+        int spot = 0;
+        g.setFont(new Font("Serif", Font.PLAIN, 30));
+        if(t.getWinner().equalsIgnoreCase(TicTacToe.O_MARKER)){
+            g.drawString("O Wins!", 200, 450);
+
+        }
+        else if (t.getWinner().equalsIgnoreCase(TicTacToe.X_MARKER)){
+            g.drawString("X Wins!", 200, 450);
+        }
+        else{
+            g.drawString("Its a Tie!", 200, 450);
+        }
+    }
+
+    public void fillWin(Graphics g, TicTacToe t){
+        g.setColor(Color.green);
+        if(isWinningSquare){
+            g.fillRect(xBuffer + (col*sideLength), yBuffer + (row*sideLength), sideLength, sideLength);
+        }
+        g.setColor(Color.black);
+        g.drawLine(xBuffer, yBuffer, xBuffer, yBuffer + (sideLength*3));
+        g.drawLine(xBuffer + sideLength, yBuffer, xBuffer + sideLength, yBuffer + (sideLength*3));
+        g.drawLine(xBuffer + (sideLength*2), yBuffer, xBuffer + (sideLength*2), yBuffer + (sideLength*3));
+
+        g.drawLine(xBuffer, yBuffer, xBuffer + (sideLength*3), yBuffer);
+        g.drawLine(xBuffer, yBuffer + sideLength, xBuffer + (sideLength*3), yBuffer + sideLength);
+        g.drawLine(xBuffer, yBuffer + (sideLength*2), xBuffer + (sideLength*3), yBuffer + (sideLength*2));
     }
 }
--- src/TicTacToeViewer.java
+++ src/TicTacToeViewer.java
@@ -5,17 +5,6 @@ public class TicTacToeViewer extends JFrame {
     // TODO: Complete this class
     private final int WINDOWN_WIDTH = 600;
     private final int WINDOOW_HEIGHT = 500;
-    private final int sideLength = 100;
-    private final int xBuffer = 100;
-    private final int yBuffer = 100;
-    private final int wBuffer = 23;
-    private final int uBuffer = 50;
-    private final int dBuffer = 150;
-
-    private final int iSize = 80;
-    private final int iBuffer = 110;
-
-
 
     Image x;
 
@@ -38,55 +27,24 @@ public class TicTacToeViewer extends JFrame {
 
     public void paint(Graphics g){
         //Draw Board
+        g.setColor(Color.black);
         b = t.getBoard();
-        g.setFont(new Font("Serif", Font.PLAIN, 30));
-        for(int i = 0; i < 3; i++){
-            g.drawString(String.valueOf(i), uBuffer, dBuffer + (yBuffer*i));
-            g.drawString(String.valueOf(i), dBuffer + (xBuffer*i),uBuffer+wBuffer);
-            for(int j = 0; j< 3; j++){
-                //Draw Images for X and O
-                g.drawRect(xBuffer + (j*sideLength),yBuffer + (i*sideLength),sideLength,sideLength);
-                if(b[i][j].getMarker().equals(TicTacToe.O_MARKER)){
-                    g.drawImage(o, iBuffer + (xBuffer*j), iBuffer + (yBuffer*i), iSize, iSize, this);
-                }
-                if(b[i][j].getMarker().equals(TicTacToe.X_MARKER)){
-                    g.drawImage(x, iBuffer + (xBuffer*j), iBuffer + (yBuffer*i), iSize, iSize, this);
-                }
+        Square.drawBoard(g);
+        for(int i = 0; i < b.length; i++){
+            for(int j = 0; j < b[i].length; j++){
+                b[i][j].drawXO(g, this);
             }
         }
         if(t.getGameOver()){
-            this.drawWin(g);
-        }
-
-
-    }
-
-    public void drawWin(Graphics g){
-        int spot = 0;
-        g.setColor(Color.green);
-        g.setFont(new Font("Serif", Font.PLAIN, 30));
-        if(t.getWinner().equalsIgnoreCase(TicTacToe.O_MARKER)){
-            g.drawString("O Wins!", 200, 450);
-            if(t.getwinDirection() == TicTacToe.COL_WIN){
-                spot = t.getwinIndex();
-                for(int i = 0; i < 3; i++){
-                    g.fillRect(xBuffer, yBuffer + (spot*sideLength), sideLength, sideLength);
+            Square.drawnWin(g, t);
+            for(int i = 0; i < b.length; i++){
+                for(int j = 0; j < b[i].length; j++){
+                    b[i][j].fillWin(g, t);
+                    b[i][j].drawXO(g, this);
                 }
             }
-            else if(t.getwinDirection() == TicTacToe.ROW_WIN){
-                spot = t.getwinIndex();
-                for(int i = 0; i < 3; i++){
-                    g.fillRect(xBuffer + (spot*sideLength), yBuffer, sideLength, sideLength);
-                }
-            }
+        }
 
-        }
-        else if (t.getWinner().equalsIgnoreCase(TicTacToe.X_MARKER)){
-            g.drawString("X Wins!", 200, 450);
-        }
-        else{
-            g.drawString("Its a Tie!", 200, 450);
-        }
 
     }
 }
